f07f8dd3a77f2ea15feb6690e6c7f737032a3998
linhaijie <138917224+linhaijie@users.noreply.github.com>
2023-07-08 05:34:21 +0800

    Add files via upload

---
+++ catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/lin/topic_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/lin/topic_ws/devel;/home/lin/demo01_ws/devel;/opt/ros/noetic'.split(';') if '/home/lin/topic_ws/devel;/home/lin/demo01_ws/devel;/opt/ros/noetic' != '' else []
---
+++ topic_vscode_c/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lin/topic_ws/src/topic_vscode_c
+# Build directory: /home/lin/topic_ws/build/topic_vscode_c
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
